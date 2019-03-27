50c0ba86590373cceb8130be60e99f7ff14ef3d9
Gustavo1877 <46923745+Gustavo1877@users.noreply.github.com>
2019-03-27 09:51:15 -0300

    Initial commit

---
+++ src/vo/Pessoa.java
@@ -0,0 +1,28 @@
+package vo;
+
+public class Pessoa {
+	
+	private String nome;
+	private String telefone;
+	private String operadoras;
+	
+	public String getOperadora() {
+		return operadoras;
+	}
+	public void setOperadora(String operadora) {
+		this.operadoras = operadora;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
